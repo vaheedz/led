4e2f341966034b017fd73ca2ff05f3b2637e554e
Vaheed Azadeh <vaheed.azadeh@gmail.com>
2015-11-27 23:02:14 -0800

    Initial commit

---
+++ Debug/blinky.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+blinky.obj: ../blinky.c
+blinky.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+blinky.obj: C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h
+
+../blinky.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h: 
